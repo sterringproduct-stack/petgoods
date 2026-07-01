096ac2e48f37273376304590faae2938654e00a9
sterringproduct-stack <sterringproduct@gmail.com>
2026-07-01 23:37:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("LIFF ทำงานแล้ว");
+const liffId = "20105673847VDFmpAY";
+
+async function main() {
+    await liff.init({ liffId });
+
+    if (!liff.isLoggedIn()) {
+        liff.login();
+        return;
+    }
+
+    const profile = await liff.getProfile();
+
+    document.getElementById("profile").innerHTML =
+        profile.displayName;
+}
+
+main();
